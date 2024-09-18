b9fc784d6f614ec6f29a75836a7bd7da82a14ac8
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2024-09-18 15:26:59 +0400

    feature: finished rough draft of delete modal styles and functionality

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,9 @@ import styles from '../styles/header.scss';
 const Header = () => {
     return (
       <header id="header">
-        <img id="logo" src='/images/header-logo.png'/>
+        <a href='/'>
+          <img id="logo" src='/images/header-logo.png'/>
+        </a>
       </header>
     );
   };
--- src/components/single-listing.js
+++ src/components/single-listing.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import styles from '../styles/single-listing.scss';
 import { priceFormatterCommaSpace } from '../util/helper';
 import CustomSlider from './slider';
+import DeleteModal from './delete-modal';
 
 const SingleListing = () => {
     const [listing, setListing] = useState(null);
@@ -13,7 +14,13 @@ const SingleListing = () => {
     const [allListingsLoading, setAllListingsLoading] = useState(true);
     const [allListingsError, setAllListingsError] = useState(null);
     const [filteredListings, setFilteredListings] = useState([]);
+    const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
     const { id } = useParams();
+
+    const navigate = useNavigate();
+    const handleRoutingToMainPage = () => {
+        navigate(`/`);
+    };
 
     useEffect(() => {
         const fetchEstate = async () => {
@@ -67,6 +74,31 @@ const SingleListing = () => {
         }
     }, [listing, allListings]);
 
+    const handleModalOpen = () => {
+        setDeleteModalIsOpen(true);
+    }
+
+    const handleModalClose = () => {
+        setDeleteModalIsOpen(false);
+    };
+
+    const handleDeleteListing = () => {
+        const deleteListing = async () => {
+            try {
+                const response = await axios.delete(`https://api.real-estate-manager.redberryinternship.ge/api/real-estates/${id}`, {
+                    headers: {
+                        'Authorization': 'Bearer 9cfda59e-ab9c-4d44-a3b0-794325f8b2e6'
+                    }
+                });
+              } catch (error) {
+                // setError(error);
+              } finally {
+                handleRoutingToMainPage();
+              }
+        };
+
+        deleteListing();
+    }
 
     if (listingLoading) {
         return <div>Loading...</div>;
@@ -76,8 +108,6 @@ const SingleListing = () => {
         return <div>Error: {listingError.message}</div>;
     }
 
-    console.log(filteredListings);
-
     const renderedSlider = filteredListings.length === 0 ? (
         <h1>i aint rendered</h1>
     ) : (
@@ -86,8 +116,9 @@ const SingleListing = () => {
 
     return (
     <div id='listing-page'>
+        <DeleteModal isOpen={deleteModalIsOpen} handleModalClose={handleModalClose} handleDelete={handleDeleteListing}/>
         <div id='back-button-holder'>
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg onClick={() => {handleRoutingToMainPage()}} width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16.9428 26.2765C16.4221 26.7972 15.5779 26.7972 15.0572 26.2765L5.72385 16.9431C5.20315 16.4224 5.20315 15.5782 5.72385 15.0575L15.0572 5.72418C15.5779 5.20349 16.4221 5.20349 16.9428 5.72418C17.4635 6.24488 17.4635 7.0891 16.9428 7.6098L9.88561 14.667H25.3333C26.0697 14.667 26.6667 15.2639 26.6667 16.0003C26.6667 16.7367 26.0697 17.3337 25.3333 17.3337H9.88561L16.9428 24.3909C17.4635 24.9115 17.4635 25.7558 16.9428 26.2765Z" fill="#021526"/>
             </svg>
         </div>
@@ -152,7 +183,7 @@ const SingleListing = () => {
                             </div>
                         </div>
                     </div>
-                    <button id='delete-listing'>ლისტინგის წაშლა</button>
+                    <button id='delete-listing' onClick={() => {handleModalOpen()}}>ლისტინგის წაშლა</button>
                 </div>
             </div>
         </div>
